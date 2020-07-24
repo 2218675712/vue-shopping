bf1b3561dade9cbbbc40e08f90a95d9f83425054
2218675712 <2218675712@qq.com>
2020-07-24 12:07:04 +0800

    2020年07月24日12:06:56 环境设置

--- src/main.js
+++ src/main.js
@@ -4,11 +4,14 @@ import VueAxios from 'vue-axios'
 import router from './router'
 import store from './store'
 import App from './App.vue'
+import env from './env'
 
 Vue.use(VueAxios, Axios)
-根据前端的跨域方式做调整
+// 根据前端的跨域方式做调整
 Axios.defaults.baseURL = '/api'
-设置请求超时
+// 根据环境变量获取不同的请求地址
+Axios.defaults.baseURL=env.baseURL
+// 设置请求超时
 Axios.defaults.timeout = 5000
 /**
  * 接口错误拦截
