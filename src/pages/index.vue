82d7b2879d54d56eea6b458f056e8b9bb3bc75a3
2218675712 <2218675712@qq.com>
2020-07-27 12:13:23 +0800

    2020年07月27日12:13:15 广告位

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -49,8 +49,16 @@
           <div class="swiper-button-next" slot="button-next"></div>
         </swiper>
       </div>
-      <div class="ads"></div>
-      <div class="banner"></div>
+      <div class="ads-box">
+        <a :href="'/#/product/'+item.id" v-for="(item,index) in adsList" :key="index">
+          <img :src="item.img">
+        </a>
+      </div>
+      <div class="banner">
+        <a href="/#/product/30">
+          <img src="/imgs/banner-1.png" alt="">
+        </a>
+      </div>
       <div class="product-box"></div>
     </div>
     <service-bar></service-bar>
@@ -113,25 +121,47 @@ export default {
 
         }
       ],
-      menuList: [[
+      menuList: [
+        [
+          {
+            id: 30,
+            img: '/imgs/item-box-1.png',
+            name: '小米CC9'
+          }, {
+            id: 31,
+            img: '/imgs/item-box-2.png',
+            name: '小米8青春版'
+          }, {
+            id: 33,
+            img: '/imgs/item-box-3.jpg',
+            name: 'Redmi K20 Pro'
+          }, {
+            id: 34,
+            img: '/imgs/item-box-4.jpg',
+            name: '移动4G专区'
+          }
+        ],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+      ],
+      adsList: [
         {
-          id: 30,
-          img: '/imgs/item-box-1.png',
-          name: '小米CC9'
-        }, {
-          id: 31,
-          img: '/imgs/item-box-2.png',
-          name: '小米8青春版'
-        }, {
           id: 33,
-          img: '/imgs/item-box-3.jpg',
-          name: 'Redmi K20 Pro'
+          img: '/imgs/ads/ads-1.png'
         }, {
-          id: 34,
-          img: '/imgs/item-box-4.jpg',
-          name: '移动4G专区'
+          id: 48,
+          img: '/imgs/ads/ads-2.jpg'
+        }, {
+          id: 45,
+          img: '/imgs/ads/ads-3.png'
+        }, {
+          id: 47,
+          img: '/imgs/ads/ads-4.jpg'
         }
-      ], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+      ]
 
     }
   }
@@ -237,6 +267,18 @@ export default {
           height: 100%;
         }
       }
+    }
+    .ads-box{
+      @include flex();
+      margin-top: 14px;
+      margin-bottom: 31px;
+      a{
+        width: 296px;
+        height: 167px;
+      }
+    }
+    .banner{
+      margin-bottom: 50px;
     }
   }
 </style>
