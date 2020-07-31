df2a72f6777a851269cee409aab870baa6e39cd2
2218675712 <2218675712@qq.com>
2020-07-31 10:55:47 +0800

    animation动画实现

--- src/pages/product.vue
+++ src/pages/product.vue
@@ -39,16 +39,14 @@
       <div class="item-video">
         <h2>60帧超慢动作摄影<br/>慢慢回味每一瞬间的精彩</h2>
         <p>后置960帧电影般超慢动作视频，将眨眼间的美妙展现得淋漓尽致！<br/>更能AI 精准分析视频内容，15个场景智能匹配背景音效。</p>
-        <div class="video-bg">
+        <div class="video-bg" @click="showSlide='slideDown'"></div>
           <div class="video-box">
-            <div class="overlay">
-            </div>
-            <div class="video">
-              <span class="icon-close"></span>
+            <div class="overlay" v-if="showSlide==='slideDown'"></div>
+            <div class="video" :class="showSlide">
+              <span class="icon-close" @click="showSlide='slideUp'"></span>
               <video src="/imgs/product/video.mp4" muted controls="controls"></video>
             </div>
           </div>
-        </div>
       </div>
     </div>
   </div>
@@ -183,21 +181,50 @@ export default {
       .video-box {
         .overlay {
           @include position(fixed);
-          background-color: #000000;
+          background-color: #333333;
           opacity: 0.4;
           z-index: 10;
 
         }
 
+        @keyframes slideDown {
+          from {
+            top: -50%;
+            opacity: 0;
+          }
+          to {
+            top: 50%;
+            opacity: 1;
+          }
+        }
+        @keyframes slideUp {
+          from {
+            top: 50%;
+            opacity: 1;
+          }
+          to {
+            top: -50%;
+            opacity: 0;
+          }
+        }
+
         .video {
           position: fixed;
-          top: 50%;
+          top: -50%;
           left: 50%;
           transform: translate(-50%, -50%);
           z-index: 10;
           width: 1000px;
           height: 536px;
           opacity: 1;
+
+          &.slideDown {
+            animation: slideDown 0.6s linear;
+            top: 50%;
+          }
+          &.slideUp {
+            animation: slideUp 0.6s linear;
+          }
 
           .icon-close {
             @include bgImg(20px, 20px, '/imgs/icon-close.png');
